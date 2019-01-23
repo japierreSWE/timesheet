67932d8bf8df6e4ddbefda48e3c3b22625959432
japierre <jean-philippe@autoreg-118062.dyn.wpi.edu>
2019-01-22 21:38:21 -0500

    Made EmployeeDAO insert periodStart into row

--- Timesheet/src/main/java/com/timeshet/db/EmployeeDAO.java
+++ Timesheet/src/main/java/com/timeshet/db/EmployeeDAO.java
@@ -1,5 +1,6 @@
 package com.timeshet.db;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 
 import com.timesheet.entity.Employee;
@@ -13,12 +14,13 @@ public class EmployeeDAO extends DAO {
 	public void createEmployee(Employee emp) throws Exception {
 		
 		try {
-			PreparedStatement ps = conn.prepareStatement("INSERT INTO Employee (employeeID, name, address, username, password) VALUES (?,?,?,?,?)");
+			PreparedStatement ps = conn.prepareStatement("INSERT INTO Employee (employeeID, name, address, username, password, periodStart) VALUES (?,?,?,?,?,?)");
 			ps.setString(1, emp.getID());
 			ps.setString(2, emp.name);
 			ps.setString(3, emp.address);
 			ps.setString(4, emp.username);
 			ps.setString(5, emp.password);
+			ps.setDate(6, Date.valueOf(emp.periodStart));
 			ps.execute();
 		} catch(Exception e) {
 			System.out.println(e.toString());
