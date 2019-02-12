8873f06183d311f31e4f47a8e1e8cecd876945ae
japierre <jean-philippe@autoreg-118062.dyn.wpi.edu>
2019-02-12 18:12:26 -0500

    Tested GetClientsHandler

--- Timesheet/src/test/java/com/timesheet/demo/GetClientsHandlerTest.java
+++ Timesheet/src/test/java/com/timesheet/demo/GetClientsHandlerTest.java
@@ -6,16 +6,26 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.junit.Assert;
 import org.junit.Test;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.google.gson.Gson;
 
 /**
  * A simple test harness for locally invoking your Lambda function handler.
  */
 public class GetClientsHandlerTest {
-
-    private static final String SAMPLE_INPUT_STRING = "{\"foo\": \"bar\"}";
-    private static final String EXPECTED_OUTPUT_STRING = "{\"FOO\": \"BAR\"}";
+	
+	Context createContext(String name) {
+		TestContext con = new TestContext();
+		con.setFunctionName(name);
+		return con;
+	}
+	
+	private static final String SAMPLE_INPUT_STRING = "{\"foo\": \"bar\"}";
 
     @Test
     public void testGetClientsHandler() throws IOException {
@@ -24,11 +34,21 @@ public class GetClientsHandlerTest {
         InputStream input = new ByteArrayInputStream(SAMPLE_INPUT_STRING.getBytes());;
         OutputStream output = new ByteArrayOutputStream();
 
-        handler.handleRequest(input, output, null);
-
-        // TODO: validate output here if needed.
+        handler.handleRequest(input, output, createContext("random"));
+        
         String sampleOutputString = output.toString();
         System.out.println(sampleOutputString);
-        Assert.assertEquals(EXPECTED_OUTPUT_STRING, sampleOutputString);
+        String body = null;
+        try {
+        	JSONParser parser = new JSONParser();
+        	JSONObject jsonResponse = (JSONObject)parser.parse(sampleOutputString);
+        	body = (String)jsonResponse.get("body");
+        } catch(Exception e) {
+        	System.out.println("problem");
+        }
+        
+        System.out.println(sampleOutputString);
+        CreateEmployeeResponse resp = new Gson().fromJson(body, CreateEmployeeResponse.class);
+        Assert.assertEquals(200, resp.httpCode);
     }
 }
