4e753254ebaf28f9a87e6de33d4e08d9e15033a5
japierre <jean-philippe@autoreg-118062.dyn.wpi.edu>
2019-03-04 11:27:34 -0500

    Made minor edit to createTimesheetHandler test

--- Timesheet/src/test/java/com/timesheet/demo/CreateTimesheetHandlerTest.java
+++ Timesheet/src/test/java/com/timesheet/demo/CreateTimesheetHandlerTest.java
@@ -33,7 +33,7 @@ public class CreateTimesheetHandlerTest {
     @Test
     public void testCreateTimesheetHandler() throws IOException {
     	
-    	Employee emp = new Employee("Test", "1 Test Way", "test", "test", LocalDate.of(2001, 1, 1));
+    	Employee emp = new Employee("Test", "1 Test Way", "test1", "test", LocalDate.of(2001, 1, 1));
         
         try {
         	new EmployeeDAO().createEmployee(emp);
